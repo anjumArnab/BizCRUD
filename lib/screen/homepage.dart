c9ff43b1db8c1d8a1d624f0521cfc8a1bf431902
anjumArnab <anjumarnab050@gmail.com>
2025-03-04 10:57:58 +0600

    search functionality added

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -13,6 +13,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Future<List<Company>?> _companyList;
+  TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
@@ -33,67 +34,105 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  Future<void> _searchCompany(String query) async {
+    setState(() {
+      _companyList = searchCompany(query);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Business Info CRUD"),
       ),
-      body: FutureBuilder<List<Company>?>(
-        future: _companyList,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (snapshot.hasError || snapshot.data == null) {
-            return const Center(
-              child: Text("Error receiving data from server"),
-            );
-          }
-
-          List<Company> data = snapshot.data!;
-
-          if (data.isEmpty) {
-            return const Center(
-              child: Text("No companies available."),
-            );
-          }
-
-          return GridView.builder(
+      body: Column(
+        children: [
+          Padding(
             padding: const EdgeInsets.all(10),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 5,
-              mainAxisSpacing: 5,
-              childAspectRatio: 1,
-            ),
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              final company = data[index];
-              return Transform.scale(
-                scale: 1.0,
-                child: GestureDetector(
-                  onLongPress: () => _deleteCompany(company.id),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => CreateCompany(company: company),
-                      ),
-                    ).then((_) => _refreshCompanyList());
-                  },
-                  child: CompanyCard(
-                    companyLogo: company.companyLogo,
-                    companyName: company.companyName,
-                    companyAddress: company.companyAddress,
-                    companyNumber: company.companyNumber,
-                  ),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search, color: Colors.grey[600]),
+                hintText: 'Search Company',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
                 ),
-              );
-            },
-          );
-        },
+              ),
+              onChanged: (value){
+                if (value.isNotEmpty) {
+                  _searchCompany(value);
+                } else {
+                  setState(() {
+                    _companyList = getAllCompanies();
+                  });
+              }
+              },
+            ),
+          ),
+            Expanded(
+              child: FutureBuilder<List<Company>?>(
+                future: _companyList,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+              
+                  if (snapshot.hasError || snapshot.data == null) {
+                    return const Center(
+                      child: Text("Error receiving data from server"),
+                    );
+                  }
+              
+                  List<Company> data = snapshot.data!;
+              
+                  if (data.isEmpty) {
+                    return const Center(
+                      child: Text("No companies available."),
+                    );
+                  }
+              
+                  return GridView.builder(
+                    padding: const EdgeInsets.all(10),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 5,
+                      mainAxisSpacing: 5,
+                      childAspectRatio: 1,
+                    ),
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      final company = data[index];
+                      return Transform.scale(
+                        scale: 1.0,
+                        child: GestureDetector(
+                          onLongPress: () => _deleteCompany(company.id),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => CreateCompany(company: company),
+                              ),
+                            ).then((_) => _refreshCompanyList());
+                          },
+                          child: CompanyCard(
+                            companyLogo: company.companyLogo,
+                            companyName: company.companyName,
+                            companyAddress: company.companyAddress,
+                            companyNumber: company.companyNumber,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
