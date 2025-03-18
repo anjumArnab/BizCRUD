48e67ef72a7b01e789aa4667a8c0cd86244d64cd
anjumArnab <anjumarnab050@gmail.com>
2025-03-18 21:36:42 +0600

    ChangeNotifierProvider implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
-import 'package:google_fonts/google_fonts.dart';
-import 'package:restapi_crud/screen/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:restapi_crud/providers/company_providers.dart';
+import 'package:restapi_crud/screen/homepage.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CompanyProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,9 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Business Info CRUD',
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme()
-      ),
+      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
       home: const HomePage(),
     );
   }
